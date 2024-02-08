b2ceab7c690eba29923c65ee1512367ee0c60a34
Edet Effiong <greatstory247@gmail.com>
2024-02-08 23:03:51 +0100

    ui modified

---
+++ New folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Skills from './components/Skills'
+import Header from './components/Header'
+import Intro from './components/Intro'
+
+export default function App() {
+  return (
+    <div>
+      {/* <Skills/> */}
+      <Header />
+      {/* <Intro/> */}
+      
+    </div>
+  )
+}
